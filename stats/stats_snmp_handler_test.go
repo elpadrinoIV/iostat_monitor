36b41c416f03f7c04fd64271e21c67c3a14fc505
Nicolas Dascanio <nicolas.dascanio@intraway.com>
2017-01-16 11:39:46 -0300

    Get next test

--- stats/stats_snmp_handler_test.go
+++ stats/stats_snmp_handler_test.go
@@ -47,6 +47,13 @@ func run_get_test(test_data []GetTestData, h *StatsSNMPHandler, t *testing.T) {
 	}
 }
 
+func run_get_next_test(test_data []GetTestData, h *StatsSNMPHandler, t *testing.T) {
+	for _, test_row := range test_data {
+		oid_res, var_type, value, err := h.GetNext(test_row.oid, false, value.MustParseOID("2"))
+		check_result(test_row.expected, ReturnData{oid_res, var_type, value, err}, t)
+	}
+}
+
 func TestGet(t *testing.T) {
 	sm := NewStatsManager()
 	sm.stats["sda"] = StatsData{"sda", 3.01, 3.02, 3.03, 3.04, 3.05, 3.06, 3.07, 3.08, 3.09, 3.10, 3.11, 3.12, 3.13}
@@ -78,4 +85,25 @@ func TestGet(t *testing.T) {
 	}
 
 	run_get_test(test_data, handler, t)
+}
+
+func TestGetNext(t *testing.T) {
+	sm := NewStatsManager()
+	sm.stats["sda"] = StatsData{"sda", 3.01, 3.02, 3.03, 3.04, 3.05, 3.06, 3.07, 3.08, 3.09, 3.10, 3.11, 3.12, 3.13}
+	sm.stats["dm-0"] = StatsData{"dm-0", 1.01, 1.02, 1.03, 1.04, 1.05, 1.06, 1.07, 1.08, 1.09, 1.10, 1.11, 1.12, 1.13}
+	sm.stats["dm-1"] = StatsData{"dm-1", 2.01, 2.02, 2.03, 2.04, 2.05, 2.06, 2.07, 2.08, 2.09, 2.10, 2.11, 2.12, 2.13}
+	sm.last_updated = time.Now()
+
+	base_oid := "1.3.6.1.3.1"
+
+	handler := NewStatsSNMPHandler(sm, base_oid)
+
+	test_data := []GetTestData{
+		{value.MustParseOID(base_oid), ReturnData{value.MustParseOID(base_oid + ".1.1"), pdu.VariableTypeInteger, 1, nil}},
+		{value.MustParseOID(base_oid + ".2"), ReturnData{value.MustParseOID(base_oid + ".2.1"), pdu.VariableTypeOctetString, "dm-0", nil}},
+		{value.MustParseOID(base_oid + ".2.1"), ReturnData{value.MustParseOID(base_oid + ".2.2"), pdu.VariableTypeOctetString, "dm-1", nil}},
+		{value.MustParseOID(base_oid + ".2.2"), ReturnData{value.MustParseOID(base_oid + ".2.3"), pdu.VariableTypeOctetString, "sda", nil}},
+	}
+
+	run_get_next_test(test_data, handler, t)
 }
